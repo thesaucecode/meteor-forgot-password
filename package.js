192a660c38c4d621f0b6881a4b00d4e823a59e6a
Elliott Spira <elliottspira@gmail.com>
2015-05-11 17:53:18 +1000

    setting version constraints on dependent packages

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
   api.use(['templating'], 'client');
-  api.use(['accounts-password', 'wylio:mandrill', 'iron:router']);
+  api.use(['accounts-password', 'wylio:mandrill@0.2.1', 'iron:router@1.0.7']);
   api.addFiles('lib/client/reset_password.html', 'client');
   api.addFiles('lib/router.js');
   api.addFiles('lib/server/forgot_password.js', 'server');
