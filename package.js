ffbb2215512a32aa9e541cab1570327578cd192f
Elliott Spira <elliottspira@gmail.com>
2015-05-11 17:48:55 +1000

    first version of the forgot-password package

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'saucecode:forgot-password',
+  version: '0.1.0',
+  summary: 'Send a custom forgot password email template using mandrill'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.0');
+  api.use(['templating'], 'client');
+  api.use(['accounts-password', 'wylio:mandrill', 'iron:router']);
+  api.addFiles('lib/client/reset_password.html', 'client');
+  api.addFiles('lib/router.js');
+  api.addFiles('lib/server/forgot_password.js', 'server');
+
+  api.export([
+    'ForgotPassword'
+  ]);
+});
