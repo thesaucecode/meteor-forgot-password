76515fc754b282e02debcfb5faf8375cd4059488
Elliott Spira <elliottspira@gmail.com>
2015-10-08 19:53:17 +1100

    Updating to latest version of wylio:mandrill

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
   name: 'saucecode:forgot-password',
-  version: '0.2.1',
+  version: '0.3.0',
   summary: 'Send a custom forgot password email template using mandrill'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
-  api.use(['accounts-password', 'wylio:mandrill@0.2.1']);
+  api.versionsFrom('METEOR@1.2');
+  api.use([
+    'accounts-password',
+    'wylio:mandrill@1.0.1'
+  ]);
   api.addFiles('lib/server/forgot_password.js', 'server');
 
   api.export([
